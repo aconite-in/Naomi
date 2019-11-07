fc88ebd877e47e6538df9705069e501296ec4600
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-07 18:46:02 +0530

    first push

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  default = "abhishek"
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "location" {
+  default = "South India"
+  description = "The Azure Region in which the resources in this example should exist"
+}
