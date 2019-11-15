62343ea816379203780b48eb121407d2bb329e55
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-15 16:30:33 +0530

    added login with az

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.15/terraform_0.12.15_linux_amd64.zip
 unzip terraform_0.12.15_linux_amd64.zip
-az login --identity -u /subscriptions/$1/resourcegroups/myRG/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myID
+az login --service-principal -u $1 -p $2 --tenant $3
 terraform init
 terraform plan -out plan.out
