7c3b80fe3b56fb9503d1d438a2902a3b1843aed5
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-15 16:07:15 +0530

    trying az identity

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.15/terraform_0.12.15_linux_amd64.zip
 unzip terraform_0.12.15_linux_amd64.zip
+az login --identity
 terraform init
 terraform plan -out plan.out
