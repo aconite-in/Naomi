2f0109dbe860940114a11d0427f689ec0291710d
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-15 15:45:09 +0530

    changed the path

--- provision.sh
+++ provision.sh
@@ -2,5 +2,6 @@ sudo apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
 unzip terraform_0.12.15_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
+cd $(System.DefaultWorkingDirectory)/_Naomi-CI/drop
 terraform init
 terraform plan -out plan.out
