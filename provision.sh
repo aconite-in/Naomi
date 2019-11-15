c4327da5c590d3abbe52a10606cd0f46b6cef32e
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-15 16:16:05 +0530

    changed

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.15/terraform_0.12.15_linux_amd64.zip
 unzip terraform_0.12.15_linux_amd64.zip
-az login --identity
+az login --identity -u /subscriptions/$1/resourcegroups/myRG/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myID
 terraform init
 terraform plan -out plan.out
