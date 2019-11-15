8ca1e93f93bf33ccec09b33bfe2fb0170e96ab6b
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-15 15:54:16 +0530

    removed moving

--- provision.sh
+++ provision.sh
@@ -1,6 +1,5 @@
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.15/terraform_0.12.15_linux_amd64.zip
 unzip terraform_0.12.15_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
 terraform init
 terraform plan -out plan.out
