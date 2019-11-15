91b0df12e5a523a2a8ef32951c2ca4352be65b2f
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-15 15:38:34 +0530

    use terra form command

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
-unzip terraform_0.12.7_linux_amd64.zip
+unzip terraform_0.12.15_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-terraform --version 
+terraform init
+terraform plan -out plan.out
