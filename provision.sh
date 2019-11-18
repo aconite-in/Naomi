49e055399e8c9cb832ae5d7965eb89d9eba75028
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-18 14:23:21 +0530

    Installation of SSH Pass

--- provision.sh
+++ provision.sh
@@ -1,6 +1,8 @@
 sudo apt-get install unzip
+sudo apt-get install sshpass
 wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip terraform_0.11.14_linux_amd64.zip
+
 export ARM_CLIENT_ID=$1
 export ARM_CLIENT_SECRET=$2
 export ARM_SUBSCRIPTION_ID=$3
