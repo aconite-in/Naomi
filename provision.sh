d7aedd072ed33ea573891773d8dceb0e32816d1f
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-18 11:40:28 +0530

    updated shell

--- provision.sh
+++ provision.sh
@@ -1,13 +1,13 @@
 sudo apt-get install unzip
-wget https://releases.hashicorp.com/terraform/0.12.15/terraform_0.12.15_linux_amd64.zip
-unzip terraform_0.12.15_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+unzip terraform_0.11.14_linux_amd64.zip
 export ARM_CLIENT_ID=$1
 export ARM_CLIENT_SECRET=$2
 export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
 terraform init
 terraform plan -out plan.out
-terraform apply plan.out -auto-approve
+terraform apply -auto-approve
 
 export vmss_ip=$(terraform output instance_ip_addr)
 echo "host1 ansible_ssh_port=50001 ansible_ssh_host=$vmss_ip" > inventory
