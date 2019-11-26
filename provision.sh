0911fb25100f2264e90b7ae9e6a1363f5f2bdb05
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-26 18:42:41 +0530

    tyring $(System.DefaultWorkingDirectory)

--- provision.sh
+++ provision.sh
@@ -20,6 +20,6 @@ echo "ansible_ssh_pass=Password1234!" >> inventory
 
 cat inventory
 export ANSIBLE_HOST_KEY_CHECKING=False
-ls
 
-sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r _aconite-in.Sonic/drop/Sonic testadmin@$vmss_ip:~/
+
+sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r $(System.DefaultWorkingDirectory)/_aconite-in.Sonic/drop/Sonic testadmin@$vmss_ip:~/
