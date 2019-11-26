10ea19e361eb154676557fcaf569fde82c9a4102
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-11-26 17:55:04 +0530

    trying to copy inventory

--- provision.sh
+++ provision.sh
@@ -22,4 +22,4 @@ cat inventory
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 
-sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r /home/vsts/work/r1/a/_aconite-in.Sonic/drop/ testadmin@$vmss_ip:~/
+sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r inventory testadmin@$vmss_ip:~/
